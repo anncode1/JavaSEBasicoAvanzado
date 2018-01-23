799a3d8ea8582447f8cf93057829b97a143127a8
Anahi Salgado <anahi@platzi.com>
2018-01-23 12:18:47 -0500

    Hola Mundo IDE Eclipse

---
+++ HolaMundo/src/com/anncode/holamundo/HolaMundo.java
@@ -0,0 +1,10 @@
+package com.anncode.holamundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo :)");
+	}
+
+}
