9ff632baa800c0b9fc798f90a857c1c7d70f3fc5
Anahi Salgado <anahi@platzi.com>
2018-01-23 13:00:02 -0500

    Tipos Flotante Texto Logico

--- HolaMundo/src/com/anncode/holamundo/HolaMundo.java
+++ HolaMundo/src/com/anncode/holamundo/HolaMundo.java
@@ -11,6 +11,32 @@ public class HolaMundo {
 		short year = -32768;
 		int id_user = 1001; 
 		long id_twitter = 12345678923456789L;
+		
+		
+		//Punto Flotante
+		float diametro = 34.25F;
+		double precio = 12345.2345678923456723456234;
+		
+		//Texto
+		char genero = 'M';
+		
+		//Logico
+		boolean isVisible = true;
+		boolean funciona = false;
+		
+		
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
