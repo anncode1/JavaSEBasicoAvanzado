1806c62e03aff56711fb7c60cc74ad1d0b2b0027
Anahi Salgado <anahi@platzi.com>
2018-01-23 12:43:20 -0500

    Tipos Enteros

--- HolaMundo/src/com/anncode/holamundo/HolaMundo.java
+++ HolaMundo/src/com/anncode/holamundo/HolaMundo.java
@@ -5,6 +5,12 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo IDE :)");
+		
+		//Enteros
+		byte edad = 127;
+		short year = -32768;
+		int id_user = 1001; 
+		long id_twitter = 12345678923456789L;
 	}
 
 }
