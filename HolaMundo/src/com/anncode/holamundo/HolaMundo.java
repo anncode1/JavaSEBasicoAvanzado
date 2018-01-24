6d792271ad90d006c7012976f912368cda44789d
Anahi Salgado <anahi@platzi.com>
2018-01-24 09:17:03 -0500

    Arrays Declaración

--- HolaMundo/src/com/anncode/holamundo/HolaMundo.java
+++ HolaMundo/src/com/anncode/holamundo/HolaMundo.java
@@ -54,6 +54,22 @@ public class HolaMundo {
 		short numero = 300;
 		byte numeroByte = (byte) numero;
 		
+		//Arrays
+		//Declaración de array
+		
+		int[] arregloInt = new int[2];
+		double arregloDouble[];
+		
+		int[][] array2D = new int[2][3];//caben 6 cupcakes
+		int[][][] array3D = new int[3][3][2]; //caben 18 cokies
+		//int[][][][] array4D = new int[1][2][3][4];
+		
+		char[][] days = { {'M','T','W'}, {'M', 'T', 'W'} };
+		
+		char[][][] letters = { { {'a','b'}, {'a','b'},{'a','b'} }, { {'a','b'}, {'a','b'}, {'a','b'} } };
+		
+		
+		
 		System.out.println(numeroByte);
 	}
 
