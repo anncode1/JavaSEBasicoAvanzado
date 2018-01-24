cb8044bc3adc0dd95585524a2f5b8390c86d4b34
Anahi Salgado <anahi@platzi.com>
2018-01-24 09:55:06 -0500

    Operadores Aritmeticos Asignacion

--- HolaMundo/src/com/anncode/holamundo/HolaMundo.java
+++ HolaMundo/src/com/anncode/holamundo/HolaMundo.java
@@ -82,6 +82,35 @@ public class HolaMundo {
 		monkey[1][0][0][1] = 'm';
 		System.out.println(monkey[1][0][0][1]);
 		
+		
+		//Operadores Aritméticos
+		int a = 1;
+		int aa = a + a;
+		System.out.println("el valor de  aa: " + aa);
+		
+		double x = 2.56;
+		int y = 9;
+		float w = (float)x + y;
+		System.out.println(w);
+		
+		System.out.println(w*2);
+		
+		double k = 4/0.00002;
+		System.out.println(k);
+		
+		System.out.println(7%2);
+		
+		
+		double f = 2;
+		int g = 3;
+		//f = f + g;
+		//f += g;
+		//f -= g;
+		//f -*= g;
+		//f /= g;
+		f %= g;
+		System.out.println(f);
+		
 		//System.out.println(numeroByte);
 	}
 
