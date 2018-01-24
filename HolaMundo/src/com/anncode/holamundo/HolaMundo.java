ade74fcac082b41ddd6510ae06b5a4e978f525f4
Anahi Salgado <anahi@platzi.com>
2018-01-24 11:04:24 -0500

    If Else Switch

--- HolaMundo/src/com/anncode/holamundo/HolaMundo.java
+++ HolaMundo/src/com/anncode/holamundo/HolaMundo.java
@@ -156,6 +156,66 @@ public class HolaMundo {
 		System.out.println("!m -> " + (!m));
 		
 		
+		//Control de Flujo
+		//If/else
+		
+		if (q > p) {
+			//true
+			System.out.println("Si es mayor");
+			
+		} else if(q == p) {
+			System.out.println("Es igual");
+		} else {
+			System.out.println("No es mayor, y no es igual");
+		}
+		
+		//Switch
+		
+		int mes = 0;
+		
+		switch (mes) {
+			case 1:
+				System.out.println("Enero");
+				break;
+			case 2:
+				System.out.println("Febrero");
+				break;
+			case 3:
+				System.out.println("Marzo");
+				break;
+			case 4:
+				System.out.println("Abril");
+				break;
+			case 5:
+				System.out.println("Mayo");
+				break;
+			case 6:
+				System.out.println("Junio");
+				break;
+			case 7:
+				System.out.println("Julio");
+				break;
+			case 8:
+				System.out.println("Agosto");
+				break;
+			case 9:
+				System.out.println("Septiembre");
+				break;
+			case 10:
+				System.out.println("Octubre");
+				break;
+			case 11:
+				System.out.println("Noviembre");
+				break;
+			case 12:
+				System.out.println("Diciembre");
+				break;
+			default:
+				System.out.println("No existe ese mes");
+				break;
+		}
+		
+		
 	}
 
 }
