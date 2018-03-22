16a189c5b133df47503397797cb08a1585eb6b83
Anahi Salgado <anahi@platzi.com>
2018-03-21 23:31:41 -0600

    SELECT con parametros

--- AmazonViewer/src/com/anncode/amazonviewer/dao/MovieDAO.java
+++ AmazonViewer/src/com/anncode/amazonviewer/dao/MovieDAO.java
@@ -33,7 +33,10 @@ public interface MovieDAO extends IDBConnection {
 						Short.valueOf(rs.getString(TMOVIE_YEAR)));
 				
 				movie.setId(Integer.valueOf(rs.getString(TMOVIE_ID)));
-
+				movie.setViewed(getMovieViewed(
+						preparedStatement, 
+						connection, 
+						Integer.valueOf(rs.getString(TMOVIE_ID))));
 				movies.add(movie);
 				
 			}
@@ -45,8 +48,30 @@ public interface MovieDAO extends IDBConnection {
 		return movies;
 	}
 	
-	private boolean getMovieViewed() {
-		return false;
+	private boolean getMovieViewed(PreparedStatement preparedStatement, Connection connection, int id_movie) {
+		boolean viewed = false;
+		String query = "SELECT * FROM " + TVIEWED +
+				" WHERE " + TVIEWED_IDMATERIAL + "= ?"+ 
+				" AND " + TVIEWED_IDELEMENT + "= ?" +
+				" AND " + TVIEWED_IDUSUARIO + "= ?";
+		ResultSet rs = null;
+		
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, ID_TMATERIALS[0]);
+			preparedStatement.setInt(2, id_movie);
+			preparedStatement.setInt(3, TUSER_IDUSUARIO);
+			
+			rs = preparedStatement.executeQuery();
+			viewed = rs.next();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+				
+		return viewed;
 	}
 	
 }
