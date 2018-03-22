ee6f32b6401582b1c6158809144627db92bfbd7a
Anahi Salgado <anahi@platzi.com>
2018-03-21 23:19:57 -0600

    Driver, Conexion DB y clase de constantes

---
+++ AmazonViewer/src/com/anncode/amazonviewer/db/DataBase.java
@@ -0,0 +1,21 @@
+package com.anncode.amazonviewer.db;
+
+public class DataBase {
+	
+	public static final String URL 		= "jdbc:mysql://localhost:3306/";
+	public static final String DB 		= "amazonviewer";
+	public static final String USER 		= "amazonviewer";
+	public static final String PASSWORD 	= "amazonviewer";
+	
+	public static final String TMOVIE 			= "movie";
+	public static final String TMOVIE_ID 		= "id";
+	public static final String TMOVIE_TITLE 		= "title";
+	public static final String TMOVIE_GENRE 		= "genre";
+	public static final String TMOVIE_CREATOR 	= "creator";
+	public static final String TMOVIE_DURATION 	= "duration";
+	public static final String TMOVIE_YEAR 		= "year";
+	
+}
+
+
+
---
+++ AmazonViewer/src/com/anncode/amazonviewer/db/IDBConnection.java
@@ -0,0 +1,24 @@
+package com.anncode.amazonviewer.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import static com.anncode.amazonviewer.db.DataBase.*;
+
+public interface IDBConnection {
+	
+	default Connection connectToDB() {
+		Connection connection = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(URL+DB, USER, PASSWORD);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally {
+			return connection;
+		}
+		
+	}
+
+}
