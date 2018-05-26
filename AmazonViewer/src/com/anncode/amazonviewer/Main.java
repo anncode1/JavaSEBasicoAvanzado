2722a89b47344efd8dcb58bd176cf5b80c1afbab
Anahi Salgado <anahi@platzi.com>
2018-05-25 19:11:35 -0500

    Lambdas Stream Filter

--- AmazonViewer/src/com/anncode/amazonviewer/Main.java
+++ AmazonViewer/src/com/anncode/amazonviewer/Main.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 import com.anncode.amazonviewer.model.Book;
 import com.anncode.amazonviewer.model.Chapter;
@@ -270,36 +272,20 @@ public class Main {
 		report.setNameFile("reporte");
 		report.setExtension("txt");
 		report.setTitle(":: VISTOS ::");
-		String contentReport = "";
+		StringBuilder contentReport = new StringBuilder();
 		
+		movies.stream().filter(m -> m.getIsViewed()).forEach(m -> contentReport.append(m.toString() + "\n"));
 		
+		Consumer<Serie> seriesEach =s -> {
+			ArrayList<Chapter> chapters = s.getChapters();
+			chapters.stream().filter(c->c.getIsViewed()).forEach(c-> contentReport.append(c.toString() + "\n"));
+		};
+		series.stream().forEach(seriesEach);
 		
-		for (Movie movie : movies) {
-			if (movie.getIsViewed()) {
-				contentReport += movie.toString() + "\n";
-				
-			}
-		}
+		books.stream().filter(b -> b.getIsReaded()).forEach(b -> contentReport.append(b.toString() + "\n"));
 		
-		for (Serie serie : series) {
-			ArrayList<Chapter> chapters = serie.getChapters();
-			for (Chapter chapter : chapters) {
-				if (chapter.getIsViewed()) {
-					contentReport += chapter.toString() + "\n";
-					
-				}
-			}	
-		}
-		
-		
-		for (Book book : books) {
-			if (book.getIsReaded()) {
-				contentReport += book.toString() + "\n";
-				
-			}
-		}
 
-		report.setContent(contentReport);
+		report.setContent(contentReport.toString());
 		report.makeReport();
 		System.out.println("Reporte Generado");
 		System.out.println();
@@ -317,32 +303,18 @@ public class Main {
 		
 		SimpleDateFormat dfNameDays = new SimpleDateFormat("E, W MMM Y");
 		dateString = dfNameDays.format(date);
-		String contentReport = "Date: " + dateString + "\n\n\n";
+		StringBuilder contentReport = new StringBuilder();
+		contentReport.append("Date: " + dateString + "\n\n\n");
 		
-		for (Movie movie : movies) {
-			if (movie.getIsViewed()) {
-				contentReport += movie.toString() + "\n";
-				
-			}
-		}
+		movies.stream().filter(m -> m.getIsViewed()).forEach(m -> contentReport.append(m.toString() + "\n"));
+		Consumer<Serie> seriesEach =s -> {
+			ArrayList<Chapter> chapters = s.getChapters();
+			chapters.stream().filter(c->c.getIsViewed()).forEach(c-> contentReport.append(c.toString() + "\n"));
+		};
+		series.stream().forEach(seriesEach);
+		books.stream().filter(b -> b.getIsReaded()).forEach(b -> contentReport.append(b.toString() + "\n"));
 		
-		for (Serie serie : series) {
-			ArrayList<Chapter> chapters = serie.getChapters();
-			for (Chapter chapter : chapters) {
-				if (chapter.getIsViewed()) {
-					contentReport += chapter.toString() + "\n";
-					
-				}
-			}
-		}
-		
-		for (Book book : books) {
-			if (book.getIsReaded()) {
-				contentReport += book.toString() + "\n";
-				
-			}
-		}
-		report.setContent(contentReport);
+		report.setContent(contentReport.toString());
 		report.makeReport();
 		
 		System.out.println("Reporte Generado");
